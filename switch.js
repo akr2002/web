de79b69c449f66408b076a9b9b7acc4149147545
Aditya <bluenerd@protonmail.com>
2025-06-11 11:52:53 +0530

    add siwtch

---
+++ switch.js
@@ -0,0 +1,12 @@
+let grade = 'B';
+
+switch (grade) {
+  case 'A':
+    console.log('Excellent');
+    break;
+  case 'B':
+    console.log('Good');
+    break;
+  default:
+    console.log('Average');
+}
