8bb8a835c4c8c71b2d0e587048240b6143795be1
Aditya <bluenerd@protonmail.com>
2025-06-11 12:25:40 +0530

    update arrow function

--- arrow.js
+++ arrow.js
@@ -1,2 +1,13 @@
 const add = (a, b) => a + b;
 console.log(add(2, 3));
+
+let name = 'John';
+
+const greet = name => `Hello ${name}`;
+console.log(greet(name));
+
+const multiply = (x, y) => {
+  let result = x * y;
+  return result;
+};
+console.log(multiply(5, 9));
