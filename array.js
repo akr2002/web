b3bfb80d6c994396e40d0e25db7cf40e2a59c8cd
Aditya <bluenerd@protonmail.com>
2025-06-11 12:19:57 +0530

    add array

---
+++ array.js
@@ -0,0 +1,8 @@
+let colors = ['red', 'green', 'blue'];
+console.log(colors);
+console.log(colors[0]);
+colors[1] = 'yellow';
+console.log('Length: ' + colors.length);
+colors.forEach(function(color) {
+  console.log(color);
+});
