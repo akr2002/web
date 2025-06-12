96255ca25599aa73c1d59cc159271762aeb060dd
Aditya <bluenerd@protonmail.com>
2025-06-12 15:14:14 +0530

    add spread

---
+++ spread.js
@@ -0,0 +1,9 @@
+let nums1 = [1, 2, 3];
+let nums2 = [4, 5];
+let combined = [...nums1, ...nums2];
+console.log(combined);
+
+let obj1 = {a: 1};
+let obj2 = {b: 2};
+let merged = {...obj1, ...obj2};
+console.log(merged);
